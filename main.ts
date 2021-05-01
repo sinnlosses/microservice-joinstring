fd4f3ec6b51c730af344509eb50466a52cf7115b
hirano <radorazoku-hawaiin@outlook.jp>
2021-05-02 03:24:25 +0900

    不具合修正
    コメント部分の処理追記

--- main.ts
+++ main.ts
@@ -6,15 +6,29 @@ function execute():void{
     const inputRows:string[] = inputText.split("\n");
     const leftString:string = getDocumentId("leftString").value;
     const rightString:string = getDocumentId("rightString").value;
+    const commentSymbol:string = getDocumentId("commentSymbol").value == "" ? "//" : getDocumentId("commentSymbol").value;
 
     for (let i = 0; i < inputRows.length; i++){
-        // 対象の行でない場合は除く
+        var commentString:string = "";
+
+        // 空白行の場合は除く
         if (!isTargetRow(inputRows[i])){
             continue; 
         }
         
+        // コメントされている行は除く
+        if (!isCommentRow(inputRows[i], commentSymbol)){
+            continue;
+        }
+        
+        // 右側にコメントがある場合、コメント文字列として退避させておく
+        var position:number = isComment(inputRows[i], commentSymbol)
+        if(position != 0){
+            commentString = inputRows[i].substr(position,inputRows[i].length)
+            inputRows[i] = (inputRows[i].replace(commentString,"").trim())
+        }        
 
-        const joinString:string = leftString + inputRows[i] + rightString
+        const joinString:string = leftString + inputRows[i] + rightString + " " + commentString
         inputRows[i] = joinString;
 
     }
@@ -33,7 +47,6 @@ function getDocumentId(id:string):HTMLInputElement{
 /**
  * 操作すべき行か判定する.
  * @param text 判定する対象テキスト
- * @param left 左側に追加したい文字列
  */
  function isTargetRow(text:string):boolean{
     if (text.length < 1){
@@ -43,4 +56,29 @@ function getDocumentId(id:string):HTMLInputElement{
         return true;
     }
     
+}
+/**
+ * コメント行か判定する.
+ * @param text 判定する対象テキスト
+ * @param commentSymbol コメント判定に使う文字列
+ */
+ function isCommentRow(text:string, commentSymbol:string):boolean{
+    if (text.trim().startsWith(commentSymbol)){
+        return false;
+    }
+    return true;
+}
+
+/**
+ * コメントか判定する.
+ * @param text 判定する対象テキスト
+ * @param commentSymbol コメント判定に使う文字列
+ */
+ function isComment(text:string, commentSymbol:string):number{
+    var position:number = 0
+    if(text.match(commentSymbol)){
+        position = text.lastIndexOf(commentSymbol)
+        return position;
+    }
+    return position;
 }
